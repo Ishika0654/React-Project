6df97db35a8aea77524640bfb7bb076c9280bc39
Ishika Singla <102655316+Ishika0654@users.noreply.github.com>
2022-06-10 11:42:16 +0530

    Add files via upload

---
+++ src/components/services.jsx
@@ -0,0 +1,28 @@
+export const Services = (props) => {
+  return (
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Our Services</h2>
+          <p>
+          We are an online travel company founded in 2000. We provide online travel services including domestic and international holiday packages, personalized travel packages, group trips, etc.
+          </p>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-4'>
+                  {' '}
+                  <i className={d.icon}></i>
+                  <div className='service-desc'>
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
